4713c4f7d261c9fbb4bf6fc64d6d86a81dcc00be
renataedit <szaboreni4@gmail.com>
2016-12-02 13:52:21 +0100

    First upload

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // Vegye ki a bower components mappából a components js és css fileokat és tegye bele a js és a css mappákba őket
+});
